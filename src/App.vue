217a69b56df794ef0ce0651f04bccbe287430f35
like7777 <70022389+like7777@users.noreply.github.com>
2021-03-22 17:37:29 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+		<div id="paypal-button-container"></div>
+  </div>
+</template>
+
+<script>
+
+import PayPal from "vue-paypal-checkout"
+import demo from './components/PayPalCheckout.vue'
+
+export default {
+  name: 'app',
+  components:{
+		demo
+	},
+	created(){
+		paypal.Buttons({
+		  createOrder: function(data, actions) {
+		    // This function sets up the details of the transaction, including the amount and line item details.
+		    return actions.order.create({
+		      purchase_units: [{
+		        amount: {
+		          value: '0.01'
+		        }
+		      }]
+		    });
+		  },
+		  onApprove: function(data, actions) {
+		    // This function captures the funds from the transaction.
+		    return actions.order.capture().then(function(details) {
+		      // This function shows a transaction success message to your buyer.
+		      alert('Transaction completed by ' + details.payer.name.given_name);
+		    });
+		  }
+		}).render('#paypal-button-container');
+	}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PayPalCheckout.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<button @click="paymentAuthorized()"></button>
+	</div>
+</template>
+
+<script>
+	import PayPal from "vue-paypal-checkout"
+	export default {
+		name:'demo',
+		data() {
+			return {
+			 credentials: {
+				sandbox: "AXFu2vIVBJKB8VzdXPUqpcfT1q_drKNkSr0M_vpFcceKo8SkR08CHfbMBCWwLcjvdm8pynCr649f9J2n",
+				production: "填写线上环境client_id"
+			 },
+			 
+			 buttonStyle: {
+				label: "pay",
+				size: "small",
+				shape: "rect",
+				color: "yellow"
+			 }
+			}
+		},
+		components: {
+			PayPal
+		},
+	 
+		methods: {
+			paymentAuthorized (data) {
+			// 授权完成的回调，可以拿到订单id
+				console.log(data);
+			},
+	
+			paymentCompleted (data) {
+				// 用户支付完成的回调，可以拿到订单id
+				console.log(data);
+			},
+	
+			paymentCancelled (data) {
+				// 用户取消交易的回调
+				console.log(data);
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
